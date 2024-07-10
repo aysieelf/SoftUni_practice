a0b0e1a6e08ee56cde5e7e16b0723892f6b7a1af
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2024-07-10 22:01:41 +0300

    Finished ex. 3 and ex. 4.

--- Fundamentals/Basic_Syntax_Conditional_Statements_Loops_moreex.py
+++ Fundamentals/Basic_Syntax_Conditional_Statements_Loops_moreex.py
@@ -1,9 +1,7 @@
 # exercise 1
 
 number_lst = [int(x) for x in input()]
-
 sorted_lst = sorted(number_lst, reverse=True)
-
 print(*sorted_lst, sep='')
 
 # exercise 2
@@ -16,3 +14,41 @@ for i in range(len(word)):
         indexes_lst.append(i)
 
 print(indexes_lst)
+
+# exercise 3
+
+sheep = input().split(", ")
+sorted_sheep = sheep[::-1]
+
+if sorted_sheep[0] == 'wolf':
+    print("Please go away and stop eating my sheep")
+else:
+    for i in range(len(sorted_sheep)):
+        if sorted_sheep[i] == 'wolf':
+            print(f"Oi! Sheep number {i}! You are about to be eaten by a wolf!")
+
+# exercise 4
+
+string = input()
+string = string.lower()
+
+count = 0
+sand = "sand"
+water = "water"
+fish = "fish"
+sun = "sun"
+for i in range(len(string) - len(sun)):
+    part = string[i:i + len(sand)]
+    if sand == part:
+        count += 1
+    part = string[i:i+len(water)]
+    if water == part:
+        count += 1
+    part = string[i:i+len(fish)]
+    if fish == part:
+        count += 1
+    part = string[i:i+len(sun)]
+    if sun == part:
+        count += 1
+
+print(count)
