4458ae369364a779abbeead13464a4d84c9189b5
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2024-07-23 17:32:05 +0300

    3 more exercises

---
+++ OOP/Encapsulation/mammal.py
@@ -0,0 +1,17 @@
+class Mammal:
+    __kingdom = "animals"
+    def __init__(self, name: str, type: str, sound: str):
+        self.name = name
+        self.type = type
+        self.sound = sound
+
+    def make_sound(self) -> str:
+        return f"{self.name} makes {self.sound}"
+
+    @classmethod
+    def get_kingdom(cls) -> str:
+        return cls.__kingdom
+
+    def info(self) -> str:
+        return f"{self.name} is of type {self.type}"
+
